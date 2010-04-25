d88c63fdf6863f5bd6a535d2a0a9e4e370ae513e
aimee <aimee@aimee-mint.(none)>
2010-04-25 12:16:05 +0100

    Install and initialize cucumber

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,3 +26,10 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
+
+config.gem "rspec", :lib => false, :version => ">= 1.2.0"
+config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
+config.gem "cucumber", :lib => false, :version => ">= 0.6.4"
+config.gem "cucumber-rails", :lib => false, :version => ">= 0.3.0"
+config.gem "capybara", :lib => false, :version => ">= 0.3.7"
+config.gem "database_cleaner", :lib => false, :version => ">= 0.5.2"
